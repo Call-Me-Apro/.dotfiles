cdfa3c4c16af62eeae869d7c24dabbd21d6fb809
Call-Me-Apro <apro@r4ilax.eu>
2021-02-24 00:38:51 +0100

    Exchanged the symlinks for the real configs

--- .scripts/i3lock-script.sh
+++ .scripts/i3lock-script.sh
@@ -1,1 +1,5 @@
-/home/apro/.dotfiles/i3/.scripts/i3lock-script.sh
+scrot /tmp/i3lock.png
+convert /tmp/i3lock.png -blur 5x4 /tmp/i3lock.png
+convert /tmp/i3lock.png /home/apro/.rice/flower-shop/lock.png -gravity center -composite -matte /tmp/i3lock.png
+i3lock -i /tmp/i3lock.png
+rm /tmp/i3lock.png
